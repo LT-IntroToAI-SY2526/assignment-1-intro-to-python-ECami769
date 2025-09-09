2bf8e77e9729940529ab3d648e83c3aa316a332b
Elisabeth Cami <ecami@cps.edu>
2025-09-09 10:38:18 -0500

    wrote down absolute

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -27,7 +27,9 @@ def absolute(n: int) -> int:
     Returns:
         the absolute value of the passed in number
     """
-    raise NotImplementedError("absolute")
+    if n < 0:
+        return -1 * n
+    return n 
 
 
 def factorial(n: int) -> int:
@@ -40,7 +42,10 @@ def factorial(n: int) -> int:
     Returns:
         factorial of the passed in number
     """
-    raise NotImplementedError("factorial")
+    result = 1
+    for num in range(1, n+1):
+        result = result * num # result *= num
+    return result 
 
 
 T = TypeVar("T")
@@ -57,8 +62,15 @@ def every_other(lst: List[T]) -> List[T]:
     Returns:
         a list of every of other item in the original list starting with the first
     """
-    raise NotImplementedError("every_other")
+    # This is the pythonic way
+    return lst[::2]
 
+    # This is the java way
+    # result = []
+    #for index in range (len(lst))
+    #    if index %2 == 0:
+    #       result.append(lst[index])
+    #return result
 
 def sum_list(lst: List[int]) -> int:
     """Takes a list of numbers, and returns the sum of the numbers in that list. Cannot
@@ -126,11 +138,11 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
 if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
-    assert every_other([1, 2, 3, 4, 5]) == [
-        1,
-        3,
-        5,
-    ], "every_other of [1,2,3,4,5] failed"
+    assert factorial (5) == 120, "factorial of 5 failed"
+    assert factorial (1) == 1, "factorial of 1 failed"
+    assert factorial (0) == 1, "factorial of 0 failed"
+    assert every_other([1, 2, 3, 4, 5]) == [1,3,5] "every_other of [1,2,3,4,5] failed"
+    assert every_other([12, 23, 35, 48, 53, 63]) == [12,35,53] "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
